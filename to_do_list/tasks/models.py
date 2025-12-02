96e5b3f0a6f1f90422003c255cbe78144a09c9c2
Tiago Martins <tiago86martins@gmail.com>
2025-12-02 14:41:33 +0000

    added class Category

--- to_do_list/tasks/models.py
+++ to_do_list/tasks/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.title
+
+
 class Project(models.Model):
 	title = models.CharField(max_lenght=200)
 	description = models.TextField()
@@ -19,7 +26,7 @@ class Task(models.Model):
 	status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='not_started')
 	due_date = models.DateTimeField()
 	project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
-	categories = models.ManyToManyField(Category, related_name='tasks') #TODO: create Category class
+	categories = models.ManyToManyField(Category, related_name='tasks')
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
