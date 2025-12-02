16ae01ec6ab4a2af8879de8beb743ba59f2499a8
Tiago Martins <tiago86martins@gmail.com>
2025-12-02 19:28:15 +0000

    created API endpoints for models

--- to_do_list/tasks/views.py
+++ to_do_list/tasks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from models import Category, Project, Task
+from serializers import ProjectSerializer, CategorySerializer, TasksSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+	queryset = Project.object.all()
+	serializer_class = ProjectSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+	queryset = Category.objects.all()
+	serializer_class = CategorySerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+	queryset = Task.objects.all()
+	serializer_class = TasksSerializer
